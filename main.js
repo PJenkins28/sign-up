702095c243d8a33dd4158856fd77981e7d9d2e43
Paige Jenkins <paigemjenkins28@gmail.com>
2023-01-24 13:44:43 -0500

    added comments and improved readability

--- main.js
+++ main.js
@@ -1,34 +1,42 @@
+
+// DOM Variables
 const passwordInitial = document.getElementById("password");
 const passwordConfirm = document.getElementById("confirm");
-const tel = document.getElementById("phone-number");
-const number = document.getElementById("number-validation");
+const noMatch = document.getElementById("password-validation"); // Password validation message
+const tel = document.getElementById("phone-number"); // Phone number input field
+const number = document.getElementById("number-validation"); // Phone number validation message
 const submit = document.getElementById("submit");
-const noMatch = document.getElementById("password-validation");
+
+
+// Verifies that both password fields match
 function matchPasswords() {
     if (passwordInitial !== passwordConfirm) {
-            noMatch.innerText = "passwords do not match";
-        }
-        else {
-            return;
-        }
+        noMatch.innerText = "passwords do not match";
+    }
+    else {
+        return;
+    }
 }
 
+// Clears password cvalidation message
 function clearMessage() {
     noMatch.innerText = '';
 }
 
 // Event Listener
 submit.addEventListener('click', function(){
-    matchPasswords()
-});
+    matchPasswords();
+})
 
-// clears confirm password
+// EL that clears password validation message when user selects either password input fields
 passwordInitial.addEventListener('click', function() {
     clearMessage();
 })
 passwordConfirm.addEventListener('click', function() {
     clearMessage();
 })
+
+// EL that displays phone number validation message
 tel.addEventListener('mouseover', () => {
     number.innerText = "Ex: 123-456-7890";
 })
