3fbbe3ef797a40d299858ee4b9f5f8fefba35188
Paige Jenkins <paigemjenkins28@gmail.com>
2023-01-24 13:13:21 -0500

    cleaened up password and telephone validation

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
 const passwordInitial = document.getElementById("password");
-const passwordConfirm = document.getElementById("password_confirm");
+const passwordConfirm = document.getElementById("confirm");
+const tel = document.getElementById("phone_number");
+const number = document.getElementById("number_validation");
 const submit = document.getElementById("submit");
+const noMatch = document.getElementById("password_validation");
 function matchPasswords() {
     if (passwordInitial !== passwordConfirm) {
-        alert("passwords do not match");
-    }
-    else {
-        return;
-    }
+            noMatch.innerText = "passwords do not match";
+        }
+        else {
+            return;
+        }
+}
+
+function clearMessage() {
+    noMatch.innerText = '';
 }
 
 // Event Listener
 submit.addEventListener('click', function(){
     matchPasswords()
 });
+
+// clears confirm password
+passwordInitial.addEventListener('click', function() {
+    clearMessage();
+})
+passwordConfirm.addEventListener('click', function() {
+    clearMessage();
+})
+tel.addEventListener('mouseover', () => {
+    number.innerText = "Ex: 123-456-7890";
+})
+tel.addEventListener('mouseout', () => {
+    number.innerText = '';
+})
